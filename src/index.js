092e3f90948470b1f642ce6e1cf4bc1e7d2303b1
TheHadiAhamdi <TheHadiAhamdi@gmail.com>
2021-10-20 23:50:25 -0700

    initialize express

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/ping', (req, res) => {
+
+	return res.send('pong');
+
+})
+
+const port = process.env.port || 3001;
+app.listen(port, () => console.log(`server is running on port ${port}`));
