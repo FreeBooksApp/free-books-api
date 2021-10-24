52c103ad5c450f672ef1ae76abb21cdbbd105707
TheHadiAhamdi <TheHadiAhamdi@gmail.com>
2021-10-23 20:23:38 -0700

    move author model from json data to prisma database

--- src/controllers/author.js
+++ src/controllers/author.js
@@ -1,21 +1,52 @@
 const prisma = require('../prisma')
 
 exports.getAuthor = (id) => {
-    
+    return prisma.authors.findUnique({
+        where: {
+            id: id
+        },
+        include: {
+            books: true
+        }
+    })
 }
 
 exports.getAuthors = () => {
-
+    return prisma.authors.findMany({
+        include: {
+            books: true
+        }
+    })
 }
 
 exports.createAuthor = (author) => {
-
+    return prisma.authors.create({
+        data: author
+    })
 }
 
 exports.updateAuthor = (id, author) => {
-
+    return prisma.authors.update({
+        where: {
+            id: id
+        },
+        data: author
+    })
 }
 
 exports.deleteAuthor = (id) => {
 
+    // first delete all of the author's books then the author
+    return prisma.$transaction([
+        prisma.books.deleteMany({
+            where: {
+                author_id: id
+            }
+        }),
+        prisma.authors.delete({
+            where: {
+                id: id
+            }
+        })
+    ])
 }
--- src/routes/authors.js
+++ src/routes/authors.js
@@ -1,39 +1,80 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
-const {getAuthor, getAuthors} = require('../controllers/author')
+const {getAuthor, getAuthors, updateAuthor, deleteAuthor} = require('../controllers/author')
 
 router.get('/', (req, res) => {
     // return list of all authors (with pagination)
-    return res.json(getAuthors())
+    getAuthors()
+        .then(authors => {
+            console.log(authors)
+            res.json(authors)
+        })
+        .catch(err => console.log(err))
 })
 
 router.post('/', (req, res) => {
-    // upload new author
+    // register new author
+    const author = req.body
+
+    createAuthor(author)
+        .then(result => {
+            console.log(result)
+            res.json(result)
+        })
+        .catch(err => console.log(err))
+
 })
 
 router.get('/:id', (req, res) => {
     // get info about single author
     const {id} = req.params
-    const id_int = parseInt(id)
+    if(!Number(id)) return res.status(400).json({message: "invalid request"})
+    getAuthor(Number(id))
+        .then(author => {
+            if(!author) {
+                return res.status(404).json({message: "Author not found"})
+            }
+            res.json(author)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({messge: "Internal Error"})
+        })
 
-    if(isNaN(id)) {
-        return res.status(400).json({message: "invalid request"})
-    }
-
-    const author = getAuthor(id_int);
-    if(!author) {
-        return res.status(404).json({message: "Author not found"})
-    }
-    return res.json(author)
 })
 
 router.put('/:id', (req, res) => {
     // update author info
+    const {id} = req.params;
+    const author = req.body;
+
+    if(!Number(id)) return res.status(400).json({message: "invalid request"})
+
+    updateAuthor(Number(id), author)
+        .then(result => {
+            console.log(result)
+            res.json(result)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: "cannot update author"})
+        })
 })
 
 router.delete('/:id', (req, res) => {
     // delete author
+    const {id} = req.params
+    if(!Number(id)) return res.status(400).json({message: "invalid request"})
+
+    deleteAuthor(Number(id))
+        .then(result => {
+            res.json({message: "author deleted successfully"})
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: "cannot delete author"})
+        })
 })
 
 module.exports = router
